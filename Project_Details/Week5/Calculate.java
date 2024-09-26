8581713e7b29bb5d7944b8fd5821f8ec70fa638d
Venkatareddy-prog <venkatareddy.gade67@gmail.com>
2024-09-26 08:05:27 +0530

    Delete Project_Details/Week5 directory

--- Project_Details/Week5/Calculate.java
+++
@@ -1,8 +0,0 @@
-package com.junitchk;
-
-public class Calculate {
-	// Method under test
-	public int add(int a, int b) {
-		return a + b;
-	}
-}
--- Project_Details/Week5/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.mockitoegg;
-
-public interface UserRepository {
-	User findById(Long id);
-}
